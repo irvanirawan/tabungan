9bcef8f92df47ad3a2e4742ee0438df334120376
irvanirawan <iranirawan1993@gmail.com>
2018-04-15 13:56:03 +0700

    oji

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    LaravelKU
+                    Laravel OJI
                 </div>
 
                 <div class="links">
